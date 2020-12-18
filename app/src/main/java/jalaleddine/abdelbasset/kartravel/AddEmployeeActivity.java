368a9ead793ca31b3f664d1b6f1c4201eebe535c
Abd <abdelbasset>
2020-12-18 05:00:15 +0200

    Add Employees

--- app/src/main/java/jalaleddine/abdelbasset/kartravel/AddEmployeeActivity.java
+++ app/src/main/java/jalaleddine/abdelbasset/kartravel/AddEmployeeActivity.java
@@ -2,20 +2,188 @@ package jalaleddine.abdelbasset.kartravel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class AddEmployeeActivity extends AppCompatActivity {
+import com.hbb20.CountryCodePicker;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static android.os.Build.VERSION_CODES.N;
+
+public class AddEmployeeActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    DatePickerDialog datePickerDialog;
+    static String LastSeen = 0 + "Day(s) ago";
+    boolean pickedDate = false;
+    boolean pickedDate2 = false;
+    EditText editTextFirstName;
+    EditText editTextMiddleName;
+    EditText editTextLastName;
+    EditText editTextPhone;
+    CountryCodePicker countryCodePicker;
+    Spinner spinnerGender;
+    Spinner positionSpinner;
+
+    EditText editTextLastSeen;
+    EditText editTextStartingDate;
+    String PickedStartingDate;
+    String PickedDOB;
+
+    EditText editTextMonthlySalary;
+    EditText editTextAddress;
+
+    EditText editTextIBANNumber;
+
+
+
+    // Connect to MSSQL
+    private static String ip = "192.168.10.102";
+    private static String port = "1433";
+    private static String forgy = "net.sourceforge.jtds.jdbc.Driver";
+    private static String database = "Customer";
+    private static String username = "sa";
+    private static String pass = "BSJProductions@2";
+    private static String url = "jdbc:jtds:sqlserver://" + ip
+            + ":" + port + "/" + database;
+    private Connection connection = null;
+
+
+    String do_it = "meow";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_employee);
+        datePickerDialog = new DatePickerDialog(
+                this, AddEmployeeActivity.this, 2020, 0, 1);
+        editTextLastSeen = findViewById(R.id.editTextLastSeen);
+        editTextStartingDate = findViewById(R.id.editTextStartingDate);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        countryCodePicker = findViewById(R.id.ccp);
+        countryCodePicker.setCountryForPhoneCode(961);
+        spinnerGender = findViewById(R.id.genderSpinner);
+        positionSpinner = findViewById(R.id.positionSpinner);
+        editTextMonthlySalary = findViewById(R.id.editTextMonthlySalary);
+        editTextAddress = findViewById(R.id.editTextAddress);
+        editTextIBANNumber = findViewById(R.id.editTextIBANNumber);
+
+
     }
 
     public void AddEmployee(View view) {
+        String gender = spinnerGender.getSelectedItem().toString();
+        String number = editTextPhone.getText().toString().trim();
+        String code = "+"+ countryCodePicker.getSelectedCountryCode() + number;
+        String postion = positionSpinner.getSelectedItem().toString();
+        String salary = editTextMonthlySalary.getText().toString().trim();
+        String address = editTextAddress.getText().toString().trim();
+        String IBAN = editTextIBANNumber.getText().toString().trim();
+        if(!pickedDate || !pickedDate2){
+            Toast.makeText(this, "Please fill all the information", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            SharedPreferences editor = getSharedPreferences("EmployeeID", MODE_PRIVATE);
+            int ID = editor.getInt("ID",101);
+
+            int realID = ID + 1;
+            SharedPreferences.Editor editor2 = getSharedPreferences("EmployeeID", MODE_PRIVATE).edit();
+            editor2.putInt("ID", realID);
+            editor2.apply();
+
+            do_it = "insert into dbo.Customer " +
+                    "(employeeID,fName,mName,lName,phoneNumber,dob,position,startingDate,monthlySalary,address,ibanNumber,employeeType,employeeGender) " +
+                    "VALUES('" + realID +"'," + "'" +phoney + "'" +"," + weighty + "," + heighty + ",'" + gender +"');";
+
+
+        }
+    }
+    public void AddDateLastSeen(View view) {
+        if(Build.VERSION.SDK_INT > N){
+            datePickerDialog.show();
+            datePickerDialog.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    String monthy = String.valueOf(month + 1);
+                    if(monthy.length() == 1){
+                        monthy = 0 + monthy;
+                    }
+                    String dayy = String.valueOf(dayOfMonth);
+                    if(dayy.length() == 1){
+                        dayy = 0 + dayy;
+                    }
+                    PickedDOB = dayy + "-" + monthy + "-" + year;
+                    editTextLastSeen.setText(PickedDOB);
+                    // LastSeenEditText.setText(milliseconds(PickedCalenderDate));
+                    pickedDate = true;
+                }
+            });
+        }
+        else{
+            Toast.makeText(this, "Saved the Date!", Toast.LENGTH_SHORT).show();
+        }
 
     }
-    //TODO: Building/Street/City
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+    }
+    private void AddtoDatabase(String full_Name,String phoney, String weighty, String heighty, String gender) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        //TODO: Inserts
+
+        try {
+            Class.forName(forgy);
+            connection = DriverManager.getConnection(url,username
+                    ,pass);
+            System.out.println("Done " + do_it);
+            Statement statement = connection.createStatement();
+            statement.execute(do_it);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addStartingDate(View view) {
+        if(Build.VERSION.SDK_INT > N){
+            datePickerDialog.show();
+            datePickerDialog.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    String monthy = String.valueOf(month + 1);
+                    if(monthy.length() == 1){
+                        monthy = 0 + monthy;
+                    }
+                    String dayy = String.valueOf(dayOfMonth);
+                    if(dayy.length() == 1){
+                        dayy = 0 + dayy;
+                    }
+                    PickedStartingDate = dayy + "-" + monthy + "-" + year;
+                    editTextStartingDate.setText(PickedStartingDate);
+                    // LastSeenEditText.setText(milliseconds(PickedCalenderDate));
+                    pickedDate2 = true;
+                }
+            });
+        }
+        else{
+            Toast.makeText(this, "Saved the Date!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
